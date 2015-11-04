2320b63ec54df93dfe49882dfa71bd53fc665ab9
snick <s_lessnick@yahoo.com>
2015-11-04 17:40:17 -0500

    added https for nixa website

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
     description='ManyToMany widget with autocomplete and sortable items.',
     keywords=['django', 'manytomany', 'm2m', 'auto complete', 'autocomplete', 'sort', 'sorted', 'widget'],
     install_requires=['django', 'django-sortedm2m'],
-    url='http://nixa.ca/',
+    url='https://nixa.ca/',
     download_url='https://github.com/nixaio/django-sorted-autocomplete-m2m/tarball/1.0.0',
 )
