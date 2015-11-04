2780298948bbf7129859f077ab1924a3ac673f9d
snick <s_lessnick@yahoo.com>
2015-11-04 15:49:19 -0500

    packaged version

---
+++ README.rst
@@ -0,0 +1,20 @@
+Django Sorted Autocomplete ManyToMany
+=====================================
+**Description:**
+    Demo will be available shortly.
+
+====
+
+**TODO:**
+    - Add layer around autocomplete dropdown to close it.
+
+====
+
+**Changelog:**
+    - v1.0.0: 
+        Packaged version. You're welcome.
+
+====
+
+**About us:**
+    We are **Nixa inc**. Visit us at http://nixa.io/
--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ from distutils.core import setup
 
 setup(
     name='django-sorted-autocomplete-m2m',
-    version='1.0.0',
-    author=u'snickk',
     packages=['sorted-autocomplete-m2m'],
-    description='',
-    zip_safe=False,
-    include_package_data=True,
+    version='1.0.0',
+    author='Simon Lessnick',
+    author_email='simon@nixa.ca',
+    company='Nixa inc.',
+    license='MIT',
+    description='ManyToMany widget with autocomplete and sortable items.',
+    keywords='django manytomany m2m auto complete autocomplete sort sorted widget',
+    install_requires=['django', 'django-sortedm2m', ],
+    url='https://github.com/nixaio/django-sorted-autocomplete-m2m',
 )
