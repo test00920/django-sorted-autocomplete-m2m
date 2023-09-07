6c520cda43911b800bf09d49a131036f5c7bb090
Lewis Eccles <lewis@boilerroom.tv>
2023-09-07 14:54:18 +0100

    updated template to include search Icon, and updated view to handle query param of limit

--- sorted_autocomplete_m2m/views.py
+++ sorted_autocomplete_m2m/views.py
@@ -4,6 +4,7 @@ __author__ = 'snake'
 
 
 def m2m_ajax(request, model, search_fields, limit=10):
+    limit = request.GET.get('limit') if request.GET.get('limit') is not None else limit
     q = request.GET.get('q')
     query_kwargs = {'%s__istartswith' % search_field: q for search_field in search_fields}
     qs = model.objects.filter(**query_kwargs).order_by(*search_fields)[:limit]
