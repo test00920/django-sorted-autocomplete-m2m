28f0afaed2e0b1f6c37a306e496f5e9a0a389e5e
Lewis Eccles <lewis@boilerroom.tv>
2023-09-08 12:08:51 +0100

    handling query param outside of reverse_lazy as it doesn't appear to take QueryStrings well and only handles path params

--- sorted_autocomplete_m2m/widgets.py
+++ sorted_autocomplete_m2m/widgets.py
@@ -7,7 +7,8 @@ from django.template.loader import get_template
 from django.utils.encoding import force_str
 from django.utils.html import conditional_escape
 from sortedm2m.forms import SortedMultipleChoiceField, SortedCheckboxSelectMultiple
-
+from urllib.parse import urlencode
+from typing import Any
 __author__ = 'snake'
 
 
@@ -22,9 +23,12 @@ class SuperSortWidget(SortedCheckboxSelectMultiple):
             'sorted-autocomplete-m2m/css/m2m.css',
         )}
 
-    def __init__(self, url_name, **kwargs):
+    def __init__(self, url_name:str, query_string:dict=None, **kwargs: Any):
         super().__init__(**kwargs)
-        self.autocomplete_url = reverse_lazy(url_name)
+
+        self.autocomplete_url = reverse_lazy(url_name) 
+        if query_string is not None:
+            self.autocomplete_url = self.autocomplete_url + '?' + urlencode(query_string)
 
     def filter_unselected_choices(self, value):
         if value is None:
